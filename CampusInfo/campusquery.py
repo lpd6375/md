c1569a87ed072bcbc24defb8ddf5ea0949b113b5
lpd6375 <lpd6375@live.com>
2019-08-07 14:58:11 +0800

    campus api design draft

---
+++ CampusInfo/campusquery.py
@@ -0,0 +1,20 @@
+# 获取校区相关信息
+from typing import List
+
+# 获取校区基本信息
+def getcampusbasicinfo()->List:
+    pass
+
+
+# 获取校区
+def getcampusdata()->List:
+    pass
+
+
+# 获取校区排名人数分布
+def getcampusrankcnt()->List:
+    pass
+
+
+# TODO:
+#     1.完成校区相关信息的展示包括学生数量、学生人数分数布、学生全国排名情况
